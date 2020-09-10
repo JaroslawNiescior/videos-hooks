32edc689cecbbf36508ae3e7ba671deebbb177f5
Jarosław Nieściór <jaroslaw.niescior@gmail.com>
2020-09-10 22:02:51 +0200

    Inital commit

---
+++ src/components/js/App.js
@@ -0,0 +1,58 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../../api/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+
+const API_KEY = "AIzaSyCltH38ZlxE_pms062kWyvIDMSqduQdHIE";
+
+class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("Hello world");
+  }
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        type: "video",
+        maxResults: 5,
+        key: API_KEY,
+      },
+    });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/js/App";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
